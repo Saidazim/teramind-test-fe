e576bd2553d99b67d8e9dc07d428b53a77fc7a5e
Saidazim <saidazim1416@gmail.com>
2022-02-27 05:43:56 +0300

    feature/handle routing and auth token

--- app/app.js
+++ app/app.js
@@ -1,13 +1,76 @@
-var appModule = angular.module("appModule", ["ngRoute"]);
+var AppModule = angular.module("AppModule", [
+  "ngStorage",
+  "ngRoute",
+  "ngFileUpload",
+]);
 
-appModule.config([
+AppModule.config([
   "$routeProvider",
   function ($routeProvider) {
-    $routeProvider.when('/files', {
-      templateUrl: 'views/files.template.html',
-      controller: 'FilesController',
-    }).otherwise({
-      redirectTo: '/files'
+    $routeProvider
+      .when("/files", {
+        templateUrl: "files/files.template.html",
+        controller: "FilesController",
+      })
+      .when("/signin", {
+        templateUrl: "auth/sign-in.template.html",
+        controller: "SignInController",
+      })
+      .when("/signup", {
+        templateUrl: "auth/sign-up.template.html",
+        controller: "SignUpController",
+      })
+      .otherwise({
+        redirectTo: "/files",
+      });
+  },
+]);
+
+AppModule.config([
+  "$httpProvider",
+  function ($httpProvider) {
+    $httpProvider.interceptors.push([
+      "$q",
+      "$location",
+      "$localStorage",
+      function ($q, $location, $localStorage) {
+        return {
+          request: function (config) {
+            config.headers = config.headers || {};
+            if ($localStorage.token) {
+              config.headers.Authorization = "Bearer " + $localStorage.token;
+            }
+            return config;
+          },
+          responseError: function (response) {
+            if (response.status === 401 || response.status === 403) {
+              $location.path("/signin");
+            }
+            return $q.reject(response);
+          },
+        };
+      },
+    ]);
+  },
+]);
+
+AppModule.run([
+  "$rootScope",
+  "$http",
+  "$location",
+  "$localStorage",
+  function ($rootScope, $http, $location, $localStorage) {
+    if ($localStorage.currentUser) {
+      $http.defaults.headers.common.Authorization =
+        "Bearer " + $localStorage.currentUser.token;
+    }
+
+    $rootScope.$on("$locationChangeStart", function (event, next, current) {
+      var publicPages = ["/signin", "/signup"];
+      var restrictedPage = publicPages.indexOf($location.path()) === -1;
+      if (restrictedPage && !$localStorage.currentUser) {
+        $location.path("/signin");
+      }
     });
   },
 ]);
